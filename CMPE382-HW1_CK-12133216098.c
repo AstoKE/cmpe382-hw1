681b258e36be59f11095a38e3bbba1276b94c663
AstoKE <astokrappersteam@gmail.com>
2026-03-17 14:57:44 +0300

    Implement functions to check for prime and abundant numbers, and update main to count them from generated files

--- CMPE382-HW1_CK-12133216098.c
+++ CMPE382-HW1_CK-12133216098.c
@@ -5,6 +5,41 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
+int isPrime(int num) {
+    int i;
+
+    if(num<2)
+        return 0;
+
+    for(i=2; i<=num/2;i++) {
+        if (num%i==0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int isAbundant(int num) {
+    int i;
+    int sum = 0;
+
+    if (num<12) {
+        return 0;
+    }
+        
+    for (i =1; i<= num/2; i++) {
+        if (num % i == 0){
+            sum += i;
+        }
+
+    }
+
+    if(sum > num) {
+        return 1;
+    }
+
+    return 0;
+}
 
 int main(){
     int N;
@@ -12,6 +47,10 @@ int main(){
     int lastThree;
     int i;
     int num;
+
+    int primeCount = 0;
+    int abundantCount = 0;
+
     FILE *file1, *file2;
 
     printf("Enter N: ");
@@ -45,7 +84,34 @@ int main(){
     fclose(file1);
     fclose(file2);
 
-    printf("File1.txt and File2.txt created successfully.\n");
+    file1 = fopen("File1.txt", "r");
+    file2 = fopen("File2.txt", "r");
+
+    if (file1 == NULL || file2==NULL) {
+        printf("Error opening files or reading.");
+
+        return 1;
+    }
+
+    while (fscanf(file1, "%d", &num)== 1) {
+        if (isPrime(num)) {
+            primeCount++;
+        }
+    }
+
+    while(fscanf(file2, "%d", &num) == 1) {
+        if (isAbundant(num)) {
+            abundantCount++;
+        }
+    }
+
+
+    fclose(file1);
+    fclose(file2);
+
+    printf("The number of positive integers in each file: %d\n", N);
+    printf("The number of prime numbers in File1: %d\n", primeCount);
+    printf("The number of abundant numbers in File2: %d\n", abundantCount);
 
     return 0;
 }
