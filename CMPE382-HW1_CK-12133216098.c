fcff42893346f1599543ebab6e5469fd697404bd
AstoKE <astokrappersteam@gmail.com>
2026-03-17 22:48:33 +0300

    Refactor main function to implement inter-process communication for counting prime and abundant numbers, and update file handling logic.

--- CMPE382-HW1_CK-12133216098.c
+++ CMPE382-HW1_CK-12133216098.c
@@ -51,7 +51,24 @@ int main(){
     int primeCount = 0;
     int abundantCount = 0;
 
-    FILE *file1, *file2;
+    int parentToPrime[2];
+    int parentToAbundant[2];
+
+    int primeToParent[2];
+    int abundantToParent[2];
+
+    int parentToPrimeResult[2];
+    int parentToAbundantResult[2];
+
+    pid_t primeProcessID, abundantProcessID;
+
+    int receivedSize;
+    int primeTotal = 0;
+    int abundantTotal = 0;
+
+
+    FILE *primeFile;
+    FILE *abundantFile;
 
     printf("Enter N: ");
     scanf("%d", &N);
@@ -62,10 +79,10 @@ int main(){
 
     srand(time(NULL));
 
-    file1 =fopen("File1.txt", "w");
-    file2 =fopen("File2.txt", "w");
+    primeFile =fopen("File1.txt", "w");
+    abundantFile  =fopen("File2.txt", "w");
 
-    if(file1== NULL || file2==NULL) {
+    if(primeFile== NULL || abundantFile==NULL) {
 
         printf("Error while opening files.\n");
         return 1;
@@ -73,46 +90,155 @@ int main(){
 
     for(i=0;i<N;i++){
         num = rand() % (max +1);
-        fprintf(file1, "%d\n", num);
+        fprintf(primeFile, "%d\n", num);
     }
 
     for (i = 0;i < N; i++) {
         num =rand() % (max +1);
-        fprintf(file2, "%d\n", num);
+        fprintf(abundantFile, "%d\n", num);
     }
 
-    fclose(file1);
-    fclose(file2);
+    fclose(primeFile);
+    fclose(abundantFile);
 
-    file1 = fopen("File1.txt", "r");
-    file2 = fopen("File2.txt", "r");
-
-    if (file1 == NULL || file2==NULL) {
-        printf("Error opening files or reading.");
-
+    if (pipe(parentToPrime) == -1 || pipe(parentToAbundant) == -1 ||
+        pipe(primeToParent) == -1 || pipe(abundantToParent) == -1 ||
+        pipe(parentToPrimeResult) == -1 || pipe(parentToAbundantResult)==-1) {
+        printf("Pipe creation failed\n");
         return 1;
     }
 
-    while (fscanf(file1, "%d", &num)== 1) {
-        if (isPrime(num)) {
-            primeCount++;
-        }
+
+    primeProcessID = fork();
+
+    if (primeProcessID <0) {
+        printf("First fork failed.\n");
+        return 1;
     }
 
-    while(fscanf(file2, "%d", &num) == 1) {
-        if (isAbundant(num)) {
-            abundantCount++;
+    if(primeProcessID == 0) {
+        close(parentToPrime[1]);
+        close(parentToAbundant[0]);
+        close(parentToAbundant[1]);
+        close(primeToParent[0]);
+        close(abundantToParent[0]);
+        close(abundantToParent[1]);
+
+        read(parentToPrime[0], &receivedSize, sizeof(int));
+
+        primeFile = fopen("File1.txt", "r");
+        if (primeFile == NULL) {
+            printf("Prime child could not open File1.txt\n");
+            exit(1);
         }
+
+        while(fscanf(primeFile, "%d", &num) == 1) {
+            if (isPrime(num)) {
+                primeTotal++;
+            }
+        }
+
+        fclose(primeFile);
+
+        write(primeToParent[1], &primeTotal, sizeof(int));
+
+        int otherResultComesFromParents;
+
+        read(parentToPrimeResult[0], &otherResultComesFromParents, sizeof(int));
+
+        if(primeTotal > otherResultComesFromParents) {
+            printf("I am child process P2: The winner is child process P2\n");
+        } else {
+            printf("I am child process p2: The winner is child process P3\n");
+        }
+
+        close(parentToPrime[0]);
+        close(primeToParent[1]);
+
+        exit(0);
+
+
     }
 
+    abundantProcessID =fork();
 
-    fclose(file1);
-    fclose(file2);
+    if (abundantProcessID < 0) {
+        printf("Second fork failed.\n");
+        return 1;
+    }
+
+    if (abundantProcessID==0) {
+        close(parentToAbundant[1]);
+        close(parentToPrime[0]);
+        close(parentToPrime[1]);
+        close(abundantToParent[0]);
+        close(primeToParent[0]);
+        close(primeToParent[1]);
+
+        read(parentToAbundant[0],&receivedSize, sizeof(int));
+
+        abundantFile = fopen("File2.txt", "r");
+        if (abundantFile == NULL) {
+            printf("Abundant child could not open File2.txt\n");
+            exit(1);
+        }
+
+        while (fscanf(abundantFile,"%d", &num) == 1) {
+            if (isAbundant(num)) {
+
+                abundantTotal++;
+            }
+        }
+
+        fclose(abundantFile);
+
+        write(abundantToParent[1], &abundantTotal, sizeof(int));
+
+        int otherResultComesFromParents;
+
+        read(parentToAbundantResult[0], &otherResultComesFromParents, sizeof(int));
+
+        if(abundantTotal > otherResultComesFromParents) {
+            printf("I am child process P3: The winner is child process P3\n");
+        } else {
+            printf("I am child process P3: The winner is child process P2\n");
+        }
+
+        close(parentToAbundant[0]);
+        close(abundantToParent[1]);
+
+        exit(0);
+    }
+
+    close(parentToPrime[0]);
+    close(parentToAbundant[0]);
+    close(primeToParent[1]);
+    close(abundantToParent[1]);
+
+    write(parentToPrime[1], &N, sizeof(int));
+    write(parentToAbundant[1], &N, sizeof(int));
+
+    read(primeToParent[0], &primeCount, sizeof(int));
+    read(abundantToParent[0], &abundantCount, sizeof(int));
+
+    write(parentToPrimeResult[1], &abundantCount, sizeof(int));
+    write(parentToAbundantResult[1], &primeCount, sizeof(int));
+
+    close(parentToPrime[1]);
+    close(parentToAbundant[1]);
+    close(primeToParent[0]);
+    close(abundantToParent[0]);
+    close(parentToPrimeResult[0]);
+    close(parentToAbundantResult[0]);
+    close(parentToPrimeResult[1]);
+    close(parentToAbundantResult[1]);
+
+    wait(NULL);
+    wait(NULL);
 
     printf("The number of positive integers in each file: %d\n", N);
     printf("The number of prime numbers in File1: %d\n", primeCount);
     printf("The number of abundant numbers in File2: %d\n", abundantCount);
 
-    return 0;
 }
 
